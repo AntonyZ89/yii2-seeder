9351f1d80e7ffc0ed5225889a9485a97f9dd3229
AntonyZ89 <antonygabriel156@hotmail.com>
2020-02-05 15:06:18 -0300

    WeightedRandom

---
+++ src/helpers/WeightedRandom.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace console\seeder\helpers;
+
+trait WeightedRandom
+{
+    /**
+     * example $weightedValues
+     *
+     * [
+     *    20 => true,
+     *    80 => false
+     * ]
+     *
+     * @param array $weightedValues
+     * @return int|string
+     */
+    static function getRandomWeightedElement(array $weightedValues) {
+        $rand = mt_rand(1, (int) array_sum($weightedValues));
+
+        foreach ($weightedValues as $key => $value) {
+            $rand -= $value;
+            if ($rand <= 0) {
+                return $key;
+            }
+        }
+    }
+}
