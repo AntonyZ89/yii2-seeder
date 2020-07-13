4b6f6e49318f66f89df10868a578792c482d7bdb
AntonyZ89 <antonyz.dev@gmail.com>
2020-07-13 20:48:48 -0300

    batch insert

--- src/TableSeeder.php
+++ src/TableSeeder.php
@@ -5,13 +5,8 @@ namespace antonyz89\seeder;
 use antonyz89\seeder\helpers\CreatedAtUpdatedAt;
 use Faker\Factory;
 use Faker\Generator;
-use Faker\Provider\pt_BR\Address;
-use Faker\Provider\pt_BR\Company;
-use Faker\Provider\pt_BR\Person;
-use Faker\Provider\pt_BR\PhoneNumber;
 use Yii;
 use yii\base\NotSupportedException;
-use yii\db\ActiveRecord;
 use yii\db\Exception;
 use yii\db\Migration;
 use yii\helpers\ArrayHelper;
@@ -35,22 +30,25 @@ abstract class TableSeeder extends Migration
     /**
      * TableSeeder constructor.
      * @param array $config
-     * @throws Exception
-     * @throws NotSupportedException
      */
     public function __construct(array $config = [])
     {
-        $this->faker = \Faker\Factory::create(str_replace('-', '_', Yii::$app->language));
+        $this->faker = Factory::create(str_replace('-', '_', Yii::$app->language));
 
         parent::__construct($config);
     }
 
+    /**
+     * @throws Exception
+     * @throws NotSupportedException
+     */
     public function __destruct()
     {
         if (!$this->skipTruncateTables) {
             $this->disableForeignKeyChecks();
-            foreach ($this->batch as $table => $values)
+            foreach ($this->batch as $table => $values) {
                 $this->truncateTable($table);
+            }
             $this->enableForeignKeyChecks();
         }
 
@@ -58,7 +56,7 @@ abstract class TableSeeder extends Migration
             $total = 0;
             foreach ($values as $columns => $rows) {
                 $total += count($rows);
-                $this->batchInsert($table, explode(',', $columns), $rows);
+                parent::batchInsert($table, explode(',', $columns), $rows);
             }
             echo "      $total row" . ($total > 1 ? 's' : null) . " inserted  in $table" . "\n";
         }
@@ -91,11 +89,13 @@ abstract class TableSeeder extends Migration
 
         $this->generate();
 
-        if (in_array('created_at', $columnNames) && !in_array('created_at', array_keys($columns)))
+        if (!array_key_exists('created_at', $columns) && in_array('created_at', $columnNames, true)) {
             $columns['created_at'] = $this->createdAt;
+        }
 
-        if (in_array('updated_at', $columnNames) && !in_array('updated_at', array_keys($columns)))
+        if (!array_key_exists('updated_at', $columns) && in_array('updated_at', $columnNames, true)) {
             $columns['updated_at'] = $this->updatedAt;
+        }
 
         $this->insertedColumns[$table] = ArrayHelper::merge(
             array_keys($columns),
@@ -105,6 +105,38 @@ abstract class TableSeeder extends Migration
         $this->batch[$table][implode(',', array_keys($columns))][] = array_values($columns);
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    public function batchInsert($table, $columns, $rows)
+    {
+        $columnNames = Yii::$app->db->getTableSchema($table)->columnNames;
+
+        $this->generate();
+
+        if (!array_key_exists('created_at', $columns) && in_array('created_at', $columnNames, true)) {
+            $columns[] = 'created_at';
+            $rows[] = $this->createdAt;
+        }
+
+        if (!array_key_exists('updated_at', $columns) && in_array('updated_at', $columnNames, true)) {
+            $columns[] = 'updated_at';
+            $rows[] = $this->updatedAt;
+        }
+
+        $this->insertedColumns[$table] = ArrayHelper::merge(
+            $columns,
+            isset($this->insertedColumns[$table]) ? $this->insertedColumns[$table] : []
+        );
+
+        foreach ($rows as $row) {
+            $this->batch[$table][implode(',', $columns)][] = $row;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     public function truncateTable($table)
     {
         $this->db = Yii::$app->db;
@@ -119,8 +151,9 @@ abstract class TableSeeder extends Migration
             $tableColumns = Yii::$app->db->getTableSchema($table)->columns;
 
             foreach ($tableColumns as $tableColumn) {
-                if (!$tableColumn->autoIncrement && !in_array($tableColumn->name, $columns))
+                if (!$tableColumn->autoIncrement && !in_array($tableColumn->name, $columns, true)) {
                     $missingColumns[$table][] = [$tableColumn->name, $tableColumn->dbType];
+                }
             }
         }
 
@@ -128,8 +161,9 @@ abstract class TableSeeder extends Migration
             echo "    > " . str_pad(' MISSING COLUMNS ', 70, '#', STR_PAD_BOTH) . "\n";
             foreach ($missingColumns as $table => $columns) {
                 echo "    > " . str_pad("# TABLE: $table", 69, ' ') . "#\n";
-                foreach ($columns as [$tableColumn, $type])
+                foreach ($columns as [$tableColumn, $type]) {
                     echo "    > " . str_pad("#    $tableColumn => $type", 69, ' ') . "#\n";
+                }
             }
             echo "    > " . str_pad('', 70, '#') . "\n";
         }
