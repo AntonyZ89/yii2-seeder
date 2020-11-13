e94b9ff2504362b2d90b8a4afc0ad261771b2fad
AntonyZ89 <antonyz.dev@gmail.com>
2020-11-13 13:42:41 -0300

    update loop function

--- functions.php
+++ functions.php
@@ -5,11 +5,13 @@ if (!function_exists('loop')) {
      * @param callable $handle
      * @param integer $count
      */
-    function loop($handle, $count)
+    function loop(callable $handle, int $count)
     {
         if ($count > 0) {
             foreach (range(1, $count) as $i) {
-                $handle($i);
+                if ($handle($i) === false) {
+                    break;
+                }
             }
         }
     }
